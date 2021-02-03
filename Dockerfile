17339944ddeac6dc00f2d76a320b79a3ae173b1d
Simon Waterhouse <simon.waterhouse@citrix.com>
2021-02-03 10:23:56 +0000

    Add support for and endpoint in the listener

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
 FROM python:3.8-buster
 
-COPY listener.py .
+WORKDIR /app
+
+COPY listener.py requirements.txt /app/
+COPY templates/ /app/templates/
+
+RUN python -m  pip install --upgrade pip && \
+    pip install -r requirements.txt
+
 CMD ["python", "-u", "listener.py"]
--- listener.py
+++ listener.py
@@ -1,8 +1,23 @@
+import logging
+import threading
+
+from flask import Flask, render_template
 from time import sleep
 
-i = 0
-while True:
-    i += 1
-    print(f'Event listener {i}')
-    sleep(5)
-    
+def listen():
+    i = 0
+    while True:
+         i += 1
+         print(f'Listening {i}')
+         sleep(5)
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template("index.html", title="Greetings", target="world")
+
+if __name__ == "__main__":
+    listener = threading.Thread(target=listen)
+    listener.start() 
+    app.run(host='0.0.0.0', port=8081)
