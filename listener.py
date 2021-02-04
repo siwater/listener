283387eb803811e68029639696c950e337b951d7
Simon Waterhouse <simon.waterhouse@citrix.com>
2021-02-04 17:57:11 +0000

    Add configurable port

--- listener.py
+++ listener.py
@@ -1,3 +1,9 @@
+#
+# Simulation of an event listen loop in a process that also exposes 
+# an HTTP endpoint (e.g. for command & control) 
+#
+import os
+import sys
 import threading
 
 from flask import Flask, render_template, jsonify
@@ -7,20 +13,25 @@ def listen():
     i = 0
     while True:
          i += 1
-         print(f'Listening {i}')
+         print(f'Listener iteration #{i}')
          sleep(5)
+
+listen_port = os.environ.get('BACKEND_PORT')
+if listen_port == None:
+    sys.exit('LISTEN_PORT undefined')
+
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template("index.html", title="Greetings", target="world")
+    return render_template("index.html", title="Greetings", target="from the listener")
 
 @app.route('/message', methods=['GET'])
 def message():
-    return jsonify({"message": "hello world"})
+    return jsonify({"message": "Listener awaiting your command"})
 
 if __name__ == "__main__":
     listener = threading.Thread(target=listen)
     listener.start() 
-    app.run(host='0.0.0.0', port=8081)
+    app.run(host='0.0.0.0', port=listen_port)
